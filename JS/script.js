455e98aa8e330b23aaf1e9d7fa996d64d3e934ea
muhahoi <kiyashko.e@gmail.com>
2023-11-01 00:06:13 +0300

    Полностью рабочий код. Дальше HTML

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,18 @@
 "use strict";
 //https://zhukovsd.github.io/java-backend-learning-course/Projects/Hangman/
-//// глобальная переменная для хранения загаданного слова
-//let randomWord = start();
-//// глобальная переменная для хранения массива звездочек
-//let starsArrow = stars();
 
 function greetings() {
 	for (let i = 1; i < 2; i++) {
-		let greetings = prompt("[N]ew game or [E]xit");
-		if (greetings == "N" || greetings == "n") {
-			console.log("Отлично");
+		let greetings = prompt("[Н]ачать новую игру или [В]ыйти из игры");
+		if (greetings == "Н" || greetings == "н") {
 			start();
-		} else if (greetings == "E" || greetings == "e") {
-			console.log("До свидания");
+		} else if (greetings == "В" || greetings == "в") {
+			alert("До свидания");
 			break;
 		} else {
-			console.log("Ошибка");
+			alert(
+				`Необходимо ввести "Н", если хотите начать игру, или "В", если хотите выйти из игры`
+			);
 			i--;
 		}
 	}
@@ -28,7 +25,6 @@ async function start() {
 	const words = text.replace(/\r/g, "").split("\n");
 	randomWord = [...words[Math.floor(Math.random() * words.length)]];
 	stars();
-	//return randomWord;
 }
 
 function stars() {
@@ -36,28 +32,79 @@ function stars() {
 	let randomWordLength = randomWord.length;
 	let stars = star.repeat(randomWordLength);
 	starsArrow = [...stars];
-	//getLetter();
-	//return starsArrow;
+	console.log(randomWord);
+	getLetter();
 }
 
-//// Выводим запрос буквы
-//function getLetter() {
-//	//let starsArrow = [];
-//	//необходимо сделать проверку на повторное введение буквы. Надо выводить сообщение, что уже есть такая буква и отматывать назад счетчик
-//	let letter = prompt("Введите букву", "");
-//	for (let i = 0; i < randomWord.length; i++) {
-//		if (letter == randomWord[i]) {
-//			starsArrow[i] = letter;
+// Выводим запрос буквы
+async function getLetter() {
+	let mistake = 0;
+	let guess = 0;
+	let inputLetterArrow = [];
+	let wrongLetterArrow = [];
+	let wrongLetterString = "";
 
-//			//randomWord[i] =
-//		}
-//	}
-//}
+	outer: for (let j = 0; j < randomWord.length + 5; j++) {
+		let letter = prompt(`Введите букву русского алфавита\n
+${randomWord}\n
+Загаданное слово: ${starsArrow.reduce((accumulator, currentValue) => {
+			return accumulator + currentValue;
+		}, "")}\n
+Неправильные буквы: ${wrongLetterString}`);
+
+		if (
+			letter == null ||
+			letter.length != 1 ||
+			letter == "" ||
+			!isNaN(letter) ||
+			!letter.match(/^[А-Яа-яёё]$/)
+		) {
+			j--;
+			continue outer;
+		}
+
+		for (let k = 0; k < inputLetterArrow.length; k++) {
+			//проверка на повтор
+			if (letter !== inputLetterArrow[k]) continue;
+			alert(`Буква "${letter}" уже была введена ранее. Попробуйте еще раз.`);
+			j--;
+			continue outer;
+		}
+
+		// Создаем флаг, который будет указывать на то, выполнено ли условное ветвление
+		let flag = false;
+		for (let i = 0; i < randomWord.length; i++) {
+			if (letter == randomWord[i]) {
+				starsArrow[i] = letter;
+				inputLetterArrow.push(letter);
+				guess++;
+				flag = true;
+			}
+		}
+
+		// Условное ветвление ни разу не выполнено
+		if (!flag) {
+			mistake++;
+			inputLetterArrow.push(letter);
+			wrongLetterArrow.push(letter);
+			wrongLetterString += `${letter} `;
+		}
+		if (mistake == 6) {
+			alert("Вы проиграли.");
+			break;
+		}
+
+		if (guess == randomWord.length) {
+			alert("Поздравляю! Вы победили!");
+			break;
+		}
+	}
+	setTimeout(greetings, 1000);
+}
+
 greetings();
+
 // глобальная переменная для хранения загаданного слова
 let randomWord;
 // глобальная переменная для хранения массива звездочек
 let starsArrow;
-//stars(start());
-//console.log("randomWord: ", randomWord);
-//console.log("stars arrow: ", starsArrow);
--- JS/test.js
+++ JS/test.js
@@ -1,53 +1,7 @@
-"use strict";
-let userName;
-let sum1 = Sum1();
-let sum2 = Sum2();
-let sum3 = Sum3();
-function greetings() {
-	for (let i = 1; i < 2; i++) {
-		userName = prompt("Привет! Как тебя зовут?", "");
+const array = [1, 2, 3, 4, 5];
 
-		if (
-			userName == NaN ||
-			userName == null ||
-			userName == undefined ||
-			userName == ""
-		) {
-			i--;
-		} else {
-			start();
-		}
-	}
+const string = array.reduce((accumulator, currentValue) => {
+	return accumulator + currentValue;
+}, "");
 
-	return userName;
-}
-
-function start() {
-	let answer = confirm(`${userName}, ты хочешь посчитать?`);
-	if (answer == true) {
-		Sum1();
-	} else {
-		alert(`Goodbuy, ${userName}!`);
-	}
-}
-function Sum1() {
-	let a = Sum2();
-	let b = Sum3();
-	let sum1 = a + b;
-	return sum1;
-}
-
-function Sum2() {
-	let c = 4;
-	let d = 5;
-	let sum2 = d - c;
-	return sum2;
-}
-
-function Sum3() {
-	return 4 + 5;
-}
-greetings();
-console.log(sum1);
-console.log(sum2);
-console.log(sum3);
+console.log(string);
